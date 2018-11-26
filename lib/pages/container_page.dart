fe0a0fdd65214256ed7aac1eca71a04c55b9c237
naimishverma17 <naimishverma50@gmail.com>
2018-11-26 15:48:55 +0530

    drawer mplementation

--- lib/pages/container_page.dart
+++ lib/pages/container_page.dart
@@ -4,15 +4,40 @@ import 'package:flutter/material.dart';
 abstract class ContainerPage extends StatelessWidget {
 // Abstract methods
   String getAppBarTitle();
+
   Widget getContentWidget();
 
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData(
+            brightness: Brightness.light,
+            primaryColor: Colors.blue,
+            accentColor: Colors.cyan),
         home: Scaffold(
-      appBar: AppBar(
-        title: Text(getAppBarTitle()),
-      ),
-      body: getContentWidget(),
-    ));
+          drawer: Drawer(
+            elevation: 20.0,
+            child: new ListView(
+              children: <Widget>[
+                new DrawerHeader(
+                  child: new Text("DRAWER HEADER.."),
+                  decoration:
+                      new BoxDecoration(color: Theme.of(context).accentColor),
+                ),
+                new ListTile(
+                  title: new Text("Item => 1"),
+                  onTap: () {},
+                ),
+                new ListTile(
+                  title: new Text("Item => 2"),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+          appBar: AppBar(
+            title: Text(getAppBarTitle()),
+          ),
+          body: getContentWidget(),
+        ));
   }
 }
