6a65ceee0e4b002a4ff57fbe08eb3613a8dc207f
naimishverma17 <naimishverma50@gmail.com>
2019-01-23 01:32:56 +0530

    faculty comp

--- lib/data/faculty_data.dart
+++ lib/data/faculty_data.dart
@@ -4,22 +4,22 @@ class FacultyData {
   static List<Faculty> _faculties = [
     new Faculty(
         name: "Dr. Sunita Yadav",
-        profileUrl: "assets/images/user.png",
+        profileUrl: "assets/images/avatar.jpeg",
         department: "CSE",
         position: "H.O.D."),
     new Faculty(
         name: "Dr. Anu Chaudhary",
-        profileUrl: "assets/images/user.png",
+        profileUrl: "assets/images/avatar.jpeg",
         department: "IT",
         position: "H.O.D."),
     new Faculty(
         name: "Mr. Deepak Rai",
-        profileUrl: "assets/images/user.png",
+        profileUrl: "assets/images/avatar.jpeg",
         department: "CSE",
         position: "Faculty Coordinator"),
     new Faculty(
         name: "Mr. Rohit Vashistha",
-        profileUrl: "assets/images/user.png",
+        profileUrl: "assets/images/avatar.jpeg",
         department: "CSE",
         position: "Faculty Coordinator")
   ];
--- lib/widgets/faculty_coordinators.dart
+++ lib/widgets/faculty_coordinators.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 
 import '../utils/constants.dart';
 import '../utils/conatus_utils.dart';
+import '../models/faculty.dart';
+import '../data/faculty_data.dart';
 
 class FacultyCoordinators extends StatelessWidget {
   Widget build(BuildContext context) {
     final double deviceWidth = MediaQuery.of(context).size.width;
+    final List<Faculty> faculty = FacultyData.facultiesData;
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage("assets/images/college.png"),
-          fit: BoxFit.cover
-        ),
+            image: AssetImage("assets/images/college.png"), fit: BoxFit.cover),
       ),
       margin: EdgeInsets.only(top: Dimensions.appBarHeight),
       padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 0.0),
@@ -20,24 +21,39 @@ class FacultyCoordinators extends StatelessWidget {
         scrollDirection: Axis.vertical,
         children: List.generate(4, (index) {
           return Container(
-            height: ConatusUtils.getCardRadius(deviceWidth)*2,
+            height: ConatusUtils.getCardRadius(deviceWidth) * 2,
             padding: EdgeInsets.symmetric(horizontal: 3.0),
             child: Card(
               color: Color(ConatusColors.lightTransparent),
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0)
-              ) ,
+                  borderRadius: BorderRadius.circular(20.0)),
               margin: EdgeInsets.symmetric(horizontal: 35.0, vertical: 20.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   CircleAvatar(
                     radius: Dimensions.avatarRadius,
-                    backgroundImage: AssetImage("assets/images/avatar.jpeg"),
+                    backgroundImage: AssetImage(faculty[index].profileUrl),
                   ),
                   Container(height: 50.0),
-                  Center(child: Text("Coordinator",style: TextStyle(fontSize: ConatusFonts.medium),)),
-                  Center(child: Text("4th year"))
+                  Center(
+                    child: Text(
+                      faculty[index].name,
+                      style: TextStyle(fontSize: ConatusFonts.medium),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      faculty[index].position,
+                      style: TextStyle(fontSize: ConatusFonts.small),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      faculty[index].department,
+                      style: TextStyle(fontSize: ConatusFonts.small),
+                    ),
+                  )
                 ],
               ),
             ),
