061a482a07ba781d3da682a34c1d3e95b9ba0f03
naimishverma17 <naimishverma50@gmail.com>
2019-01-26 22:40:15 +0530

    title animation added

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/animation.dart';
 import '../utils/constants.dart';
 import './tagline.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   static const String _ABOUT =
       """Leading the technical upfront, Conatus is the Computer Science and Information Technology society of Ajay Kumar Garg Engineering College. "Conatus" means "impulsive force" as it brings the students to the unexplored "technical horizon" within their grasp. Conatus stands on the objective of guiding the students towards the dawn of a better and successful tomorrow with its marvellous events, workshops, seminars and classes for the students of the college to thrive towards a brighter and a technically sound future.""";
+  Animation _animation;
+  AnimationController _animationController;
+
+  initState() {
+    super.initState();
+    _animationController = AnimationController(
+        duration: const Duration(milliseconds: 4000), vsync: this);
+    _animation = Tween(begin: 250.0, end: 400.0).animate(_animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _animationController.forward();
+      }
+    });
+    _animationController.forward();
+  }
 
   Widget build(BuildContext context) {
     final double _height = MediaQuery.of(context).size.height;
@@ -23,9 +48,13 @@ class Home extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Image(
-                image: AssetImage("assets/images/conatua-text.png"),
-                height: 90,
+              SizedBox(
+                height: 100.0,
+                child: Image(
+                  image: AssetImage("assets/images/conatua-text.png"),
+                  height: _animation.value / 3,
+                  width: _animation.value,
+                ),
               ),
               SizedBox(
                 height: Dimensions.gap,
