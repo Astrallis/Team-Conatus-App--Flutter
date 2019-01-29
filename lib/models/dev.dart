d96348fffc2bae642b51fd504bc1e18adfa2d10e
naimishverma17 <naimishverma50@gmail.com>
2019-01-29 21:39:49 +0530

    images and model added

---
+++ lib/models/dev.dart
@@ -0,0 +1,9 @@
+import 'package:meta/meta.dart';
+
+class Dev {
+  String name;
+  String tech;
+  String imageUrl;
+
+  Dev({@required this.name, @required this.tech, @required this.imageUrl}) {}
+}
