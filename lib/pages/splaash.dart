d74d775df7fa8c0dcfcf79150dd61f39d18bda21
Sparsh Gupta <kbsg008@gmail.com>
2019-01-30 14:20:03 +0530

    SPLASH Resize and Repositioned

--- lib/pages/splaash.dart
+++ lib/pages/splaash.dart
@@ -9,16 +9,21 @@ class SplaashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Connection.setUpConnectionListener();
-    return Scaffold(
-      body: SplashScreen(
-          seconds: 6,
-          navigateAfterSeconds:HomePage(),
+    return Scaffold(backgroundColor: Colors.black,
+      body: Container(
+        margin: EdgeInsets.fromLTRB(60,210,0,0),
+        height: 300,width: 300,
+        child: SplashScreen(
+
+            seconds: 6,
+            navigateAfterSeconds:HomePage(),
 //          image: new Image.asset("assets/images/logo-gif.gif",width:600.0,),
-          imageBackground: AssetImage("assets/images/logo-gif.gif"),
-          backgroundColor: Colors.black,
-          styleTextUnderTheLoader: new TextStyle(),
-          photoSize: 80.0,
-          loaderColor: Colors.black),
+            imageBackground: AssetImage("assets/images/logo-gif.gif"),
+            backgroundColor: Colors.black,
+            styleTextUnderTheLoader: new TextStyle(),
+            photoSize: 80.0,
+            loaderColor: Colors.black),
+      ),
     );
   }
 }
